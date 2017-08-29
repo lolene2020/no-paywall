bbc9abdbea5a22770e856067286a0c59880f30a6
Gustav Öst <gustav.ost@snowsoftware.com>
2017-08-30 01:04:13 +0200

    Removed some DN login stuff

--- No Paywall/dn-paywall-remover.js
+++ No Paywall/dn-paywall-remover.js
@@ -5,3 +5,5 @@ addGlobalStyle('div.paywall { display: none !important; }');
 addGlobalStyle('div.ad { display: none !important; }');
 addGlobalStyle('.article__body--mask .article__lead { position: initial !important; max-height: 1000px !important; }');
 addGlobalStyle('div.ad--match-ad { display: none !important; }');
+addGlobalStyle('div.account-information { display: none !important; }');
+addGlobalStyle('div.navigation__sticky-container--sticky { display: none !important; }');
