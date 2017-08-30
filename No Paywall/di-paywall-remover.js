106dfe7408c2e589fa2f320b38a5aac0cef01157
Gustav Öst <gustav.ost@snowsoftware.com>
2017-08-30 02:29:04 +0200

    Shortend the CSS-rules

--- No Paywall/di-paywall-remover.js
+++ No Paywall/di-paywall-remover.js
@@ -1,8 +1,4 @@
+// None
+addGlobalStyle('div#serviceplusPaywallpaywall-container, iframe, div.di_panorama-wrapper, .di_enrichment--ad, div.ad_container, a[href*="/sponsrat/"], a[href*="kampanj.di.se"] { display: none !important; } ');
+// Other
 addGlobalStyle('div.paywall-content { height: auto !important; }');
-addGlobalStyle('div#serviceplusPaywallpaywall-container { display: none !important; }');
-addGlobalStyle('iframe { display: none !important; }');
-addGlobalStyle('div.di_panorama-wrapper { display: none !important; }');
-addGlobalStyle('.di_enrichment--ad { display: none !important; }');
-addGlobalStyle('div.ad_container { display: none !important; }');
-addGlobalStyle('a[href*="/sponsrat/"] { display: none !important; }');
-addGlobalStyle('a[href*="kampanj.di.se"] { display: none !important; }');
--- No Paywall/dn-paywall-remover.js
+++ No Paywall/dn-paywall-remover.js
@@ -1,9 +1,6 @@
-addGlobalStyle('.article__body--mask .article__premium-content { display: block !important; }');
-addGlobalStyle('.article__body--mask .article__premium-content > * { display: block !important; }');
-addGlobalStyle('div.factbox { display: block !important; }');
-addGlobalStyle('div.paywall { display: none !important; }');
-addGlobalStyle('div.ad { display: none !important; }');
-addGlobalStyle('.article__body--mask .article__lead { position: initial !important; max-height: 1000px !important; }');
-addGlobalStyle('div.ad--match-ad { display: none !important; }');
-addGlobalStyle('div.account-information { display: none !important; }');
-addGlobalStyle('div.navigation__sticky-container--sticky { display: none !important; }');
+// None
+addGlobalStyle('div.ad, div.paywall, div.ad--match-ad, div.account-information, div.navigation__sticky-container--sticky  {	 display: none !important; } ');
+// Block
+addGlobalStyle('div.factbox, .article__body--mask, .article__premium-content, .article__premium-content > * { display: block !important; } ');
+// Other
+addGlobalStyle('.article__body--mask .article__lead { position: initial !important; max-height: 1000px !important; } ');
